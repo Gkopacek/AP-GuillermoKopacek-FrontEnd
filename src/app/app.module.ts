6954e6cecf5bfefc57cba3da069db940c42fc4c0
Guillermo Kopacek <Kopacek5@gmail.com>
2022-08-11 18:08:05 -0300

    generacion del componente (baner.component) via CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BanerComponent } from './components/baner/baner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    BanerComponent
   ],
   imports: [
     BrowserModule
