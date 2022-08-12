9a3b33428d349d087c89a3f2e44ac8258163ef10
Guillermo Kopacek <Kopacek5@gmail.com>
2022-08-12 01:29:08 -0300

    generacion del componente (info.component) via CLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BanerComponent } from './components/baner/baner.component';
+import { InfoComponent } from './components/info/info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    BanerComponent
+    BanerComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
