23b4f08cbe8cab0b082d83e75d3e43805ca841bd
Guillermo Kopacek <Kopacek5@gmail.com>
2022-08-18 16:04:04 -0300

    se añadieron datos de educacion y trabajo a los componentes experiencia y educacion

--- src/app/model/educacion.model.ts
+++ src/app/model/educacion.model.ts
@@ -2,10 +2,12 @@ export class Educacion{
     nombre_institucion: string;
     carrera: string;
     periodo: string;
+    img: string;
     
-    constructor(nombre_institucion: string, carrera: string, periodo: string) {
+    constructor(nombre_institucion: string, carrera: string, periodo: string, img: string) {
         this.nombre_institucion = nombre_institucion;
         this.carrera = carrera;
         this.periodo = periodo;
+        this.img = img;
     }
 }
--- src/app/model/trabajo.model.ts
+++ src/app/model/trabajo.model.ts
@@ -2,10 +2,12 @@ export class Trabajo{
     nombre_empresa: string;
     tarea: string;
     periodo: string;
+    img: string;
     
-    constructor(nombre_empresa: string, tarea: string, periodo: string) {
+    constructor(nombre_empresa: string, tarea: string, periodo: string, img: string) {
         this.nombre_empresa = nombre_empresa;
         this.tarea = tarea;
         this.periodo = periodo;
+        this.img = img;
     }
 }
